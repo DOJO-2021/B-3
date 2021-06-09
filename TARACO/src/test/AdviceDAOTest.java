635a4169d70f484a5eae96182ce4a25fb4447eb4
dojo2021-miyamoto-k <miyamoto-k@seplus2016.onmicrosoft.com>
2021-06-09 17:52:42 +0900

    6/9分の書類進捗とプロジェクトのフォルダとファイルの追加(jsとcssはまだ)

---
+++ TARACO/src/test/AdviceDAOTest.java
@@ -0,0 +1,10 @@
+package test;
+
+public class AdviceDAOTest {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ TARACO/src/test/AnswerDAOTest.java
@@ -0,0 +1,10 @@
+package test;
+
+public class AnswerDAOTest {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
