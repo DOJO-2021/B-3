85a8de5de9318ce62f5b02518384fcf53b614a88
dojo2021-m-tanabe <tanabe@seplus2016.onmicrosoft.com>
2021-06-23 16:27:58 +0900

    0623

--- TARACO/WebContent/js/login.js
+++ TARACO/WebContent/js/login.js
@@ -6,7 +6,7 @@ document.getElementById('form').onsubmit = function(event) {
 	const user_pw = document.getElementById('form').USER_PW.value;
 
 	// 半角英数字8~16文字チェック
-	if (!(user_id.match(/^([a-zA-Z0-9]{8,16})$/) || user_pw.match(/^([a-zA-Z0-9]{8,16})$/))) {
+	if (!(user_id.match(/^([a-zA-Z0-9]{8,16})$/) && user_pw.match(/^([a-zA-Z0-9]{8,16})$/))) {
 		event.preventDefault();
 		document.getElementById('errormsg').textContent = `※IDとPWを半角英数字8～16文字で入力してください`;
 	}
